151d01bb269a55c98255e5aa94772ad7fdaa3fd5
ifournight <ifournight@gmail.com>
2017-03-07 22:12:26 +0800

    reset db, use devise migrate

--- db/migrate/20170306102157_create_users.rb
+++ db/migrate/20170306102157_create_users.rb
@@ -1,16 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :email
-      t.text   :website
-      t.text   :intro
-      t.string :password_digest # required by has_secure_password
+      t.string :name, null: false, default: ''
 
       t.timestamps
 
-      t.index :name
-      t.index :email
+      t.index :name, unique: true
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170307024808) do
+ActiveRecord::Schema.define(version: 20170307135013) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.text     "website"
-    t.text     "intro"
-    t.string   "password_digest"
-    t.datetime "created_at",                        null: false
-    t.datetime "updated_at",                        null: false
-    t.boolean  "activated",         default: false
-    t.datetime "activated_at"
-    t.string   "activation_digest"
-    t.index ["email"], name: "index_users_on_email"
-    t.index ["name"], name: "index_users_on_name"
+    t.string   "name",                   default: "", null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
