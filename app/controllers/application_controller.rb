88cb75de68c12bc2300fe770a2860c8b89cd1f58
ifournight <ifournight@gmail.com>
2017-03-08 10:50:10 +0800

    dynamic layout, temp root page with sign_out link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  layout :dynamic_layout
+
+
+  private
+
+  def dynamic_layout
+    case controller_name
+    when 'registrations'
+      'signup'
+    when 'sessions'
+      'signup'
+    when 'passwords'
+      'signup'
+    else
+      'application'
+    end
+  end
 end
