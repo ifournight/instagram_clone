89552f97560cf02ad698815c710de419af1c3abf
ifournight <ifournight@gmail.com>
2017-03-15 23:29:24 +0800

    Notice minor tweak

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -21,12 +21,12 @@ class Users::PasswordsController < Devise::PasswordsController
 
   # protected
 
-  # def after_resetting_password_path_for(resource)
-  #   super(resource)
-  # end
+  def after_resetting_password_path_for(resource)
+    sign_in_url
+  end
 
   # The path used after sending reset password instructions
-  # def after_sending_reset_password_instructions_path_for(resource_name)
-  #   super(resource_name)
-  # end
+  def after_sending_reset_password_instructions_path_for(resource_name)
+    sign_in_url
+  end
 end
