4f6b0b44be3d65ce59644a2f94d4ce3f73f9fbc3
ifournight <ifournight@gmail.com>
2017-03-15 23:56:38 +0800

    use resize_to_fill to create versions of posts' picture, so we can use square pictures.

--- app/uploaders/post_picture_uploader.rb
+++ app/uploaders/post_picture_uploader.rb
@@ -34,11 +34,11 @@ class PostPictureUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process resize_to_fit: [50, 50]
+    process resize_to_fill: [50, 50]
   end
 
   version :middle do
-    process resize_to_fit: [400, 400]
+    process resize_to_fill: [400, 400]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
