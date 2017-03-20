c17d80f50e5050338b26ad66e0e23c31a97423d5
ifournight <ifournight@gmail.com>
2017-03-20 17:50:31 +0800

    Add locale file  for model/ model attrs

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,8 @@ module InstagramClone
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # 指定 I18n 库搜索翻译文件的路径
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
   end
 end
--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,5 +1,5 @@
 # 指定 I18n 库搜索翻译文件的路径
-I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*{.rb, .yml}')]
+I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*{.rb, .yml}')]
 
 # 手动安装zh-CN
 I18n.available_locales = [:en, 'zh-CN']
