48bac9a6c50d05d9edc7b2c511c811b99f6bd5ef
ifournight <ifournight@gmail.com>
2017-03-20 17:01:53 +0800

    Manually add zh-CN locale, and set as default

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,5 +1,9 @@
 # 指定 I18n 库搜索翻译文件的路径
 I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*{.rb, .yml}')]
 
+# 手动安装zh-CN
+I18n.available_locales = [:en, 'zh-CN']
+
 # 修改默认区域配置（默认是 :en)
-I18n.default_locale = :en
+
+I18n.default_locale = 'zh-CN'
