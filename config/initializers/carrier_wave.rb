7951dea691faf749373002c261353b7c77b0176b
ifournight <ifournight@gmail.com>
2017-03-23 15:44:52 +0800

    Try another fog-google credential config

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,4 +1,4 @@
-if Rails.env.test? || Rails.env.cucumber?
+if Rails.env.test? || Rails.env.development?
   CarrierWave.configure do |config|
     config.storage = :file
     config.enable_processing = false
@@ -7,11 +7,12 @@ end
 
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.fog_provider = 'fog/google'                        # required
+    config.fog_provider = 'fog/google'
     config.fog_credentials = {
-      provider:                         'Google',
-      google_storage_access_key_id:     ENV['GCLOUD_ACCESS_ID'],
-      google_storage_secret_access_key: ENV['GCLOUD_SECRET_KEY']
+      provider: 'Google',
+      google_project: ENV['GCLOUD_GOOGLE_PROJECT'],
+      google_client_email: ENV['GCLOUD_CLIENT_EMAIL'],
+      google_json_key_location: Rails.root.join('.fog/key.json')
     }
     config.fog_directory = ENV['GCLOUD_BUCKET']
   end
