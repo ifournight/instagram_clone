784491c44afeda2ec8162040faaacadc75a05e4b
ifournight <ifournight@gmail.com>
2017-03-23 14:54:23 +0800

    Just use original filename in post_picture_uploader
    Because requirement of Google Cloud Storage Object Naming. The forename
    generated by code `Base64.encode64` is illegal.
    I can’t figure out how it is illegal, to make it work, just don’t
    override the `filename` method to let it work.

--- app/uploaders/post_picture_uploader.rb
+++ app/uploaders/post_picture_uploader.rb
@@ -52,8 +52,8 @@ class PostPictureUploader < CarrierWave::Uploader::Base
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  def filename
-    Base64.encode64(original_filename) if original_filename
-  end
+  # def filename
+  #   original_filename
+  # end
 
 end
